b13ea7ca3d0de8887270e10ddf520be6a1b96ed8
max.m <maxim@miholap.com>
2015-04-01 18:21:41 +0300

    in progress

---
+++ src/com/miholap/dependencyinjection/Util.java
@@ -0,0 +1,12 @@
+package com.miholap.dependencyinjection;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by user on 4/1/2015.
+ */
+public class Util {
+    static Method getSetter(String fieldName, Class objectClass){
+        return null;
+    }
+}
--- src/com/miholap/dependencyinjection/injector/Factories/ApplicationObjectFactory.java
+++ src/com/miholap/dependencyinjection/injector/Factories/ApplicationObjectFactory.java
@@ -1,9 +1,15 @@
 package com.miholap.dependencyinjection.injector.Factories;
 
+import com.miholap.dependencyinjection.consumer.Consumer;
 import com.miholap.dependencyinjection.injector.Context.BeanDefinitionReader;
 import com.miholap.dependencyinjection.injector.Context.Definition;
+import com.miholap.dependencyinjection.service.MessageService;
 
+import java.lang.reflect.Method;
 import java.util.Map;
+import java.util.Set;
+
+import static org.reflections.ReflectionUtils.*;
 
 public class ApplicationObjectFactory extends ObjectFactory {
 
@@ -14,7 +20,33 @@ public class ApplicationObjectFactory extends ObjectFactory {
     @Override
     public Object getBean(String beanName) {
         Definition definition = getDefinitionByBeanName(beanName);
+        final Class objectClass = definition.getObjectClass();
+        final Map<String,Object> properties = definition.getProperties();
+        Object initializedObject = null;
+        try {
+            initializedObject = objectClass.newInstance();
+            if (properties.size() > 0 ) {
+                properties.forEach( (fieldName,value) -> {
+/*                    try {
+                        objectClass.getDeclaredField(fieldName).getS;
+                    } catch (NoSuchFieldException e) {
+                        e.printStackTrace();
+                    }*/
+                });
 
+                Set<Method> objectSetters = getAllMethods(objectClass,
+                        withPrefix("set"),
+                        withParametersCount(1));
+                if (objectSetters.size() > 0) {
+
+                } else return null;
+
+            } else{
+                return initializedObject;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
